d448e47e1de621291a97e47968d9281c711ea136
JaAncaten <137181925+JaAncaten@users.noreply.github.com>
2023-06-28 10:24:25 -0400

    Add files via upload

---
+++ Eavaluacion3.py
@@ -0,0 +1,43 @@
+import os
+
+PrecioM = [2000 , 9999 ]
+Peso= [0.1 , 20]
+
+
+
+
+
+Menu = """
+  -------Menu-------
+  1.- Grabar 
+  2.- Buscar 
+  3.- Listar Encomiendas
+  4.- Salir 
+"""
+
+while True:
+    try:
+        opc = int(input(Menu))
+        
+        if opc == 1: 
+            input("Tipo Paquete")
+            input("Nombre Destinatario:") 
+            input("Rut Destinatario. ")
+            input("Peso Paquete:")
+            input("Ciudad Destino ")
+            
+        elif opc == 4:
+            Print 
+           
+           
+            
+
+    
+    
+    except:
+        print("Opcion Erronea ")
+        
+        
+
+
+    
